0668a8c0e09c8613ea9fe689f193a3b769462b6b
Wesley <wesley.bekkers@gmail.com>
2018-02-22 15:27:19 +0100

    #Feature1 add Log4j to file

--- src/main/java/com/yacht/MyCrawler.java
+++ src/main/java/com/yacht/MyCrawler.java
@@ -9,18 +9,29 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class MyCrawler {
+    
+    private static Logger logger = LoggerFactory.getLogger(MyCrawler.class);
+
     public static void main(String[] args) {
+
         System.out.println("This is my crawler");
-        Logger logger = LoggerFactory.getLogger(MyCrawler.class);
-        logger.info("Hello this in My CrawlerLogging");
+
+            logger.trace("Trace Message!");
+            logger.debug("Debug Message!");
+            logger.info("Info Message!");
+            logger.warn("Warn Message!");
+            logger.error("Error Message!");
 
         basic();
 
-//        mvn clean compile exec:java -Dexec.mainClass="com.yacht.MyCrawler" -Dorg.slf4j.simpleLogger.defaultLogLevel=INFO
     }
+
+//        mvn clean compile exec:java -Dexec.mainClass="com.yacht.MyCrawler" -Dorg.slf4j.simpleLogger.defaultLogLevel=INFO
+
+
     private static void basic() {
         List<? extends SiteCrawlerAction> siteCrawlerActions = new LinkedList<>();
-        SiteCrawler siteCrawler = new SiteCrawler("www.ziggo.nl", "https://www.ziggo.nl", siteCrawlerActions);
+        SiteCrawler siteCrawler = new SiteCrawler("http://www.yacht.nl", "https://www.yacht.nl", siteCrawlerActions);
         siteCrawler.getAllowedSuffixes().add("");
         siteCrawler.navigate();
         siteCrawler.disableCrawling();
