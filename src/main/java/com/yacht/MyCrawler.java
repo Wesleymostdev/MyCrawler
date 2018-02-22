3912f99000df584c47ee1e28474d325c1f6f872a
Wesley <wesley.bekkers@gmail.com>
2018-02-22 16:00:25 +0100

    #Feature 1 log4j to file

--- src/main/java/com/yacht/MyCrawler.java
+++ src/main/java/com/yacht/MyCrawler.java
@@ -13,9 +13,7 @@ public class MyCrawler {
     private static Logger logger = LoggerFactory.getLogger(MyCrawler.class);
 
     public static void main(String[] args) {
-
         System.out.println("This is my crawler");
-
             logger.trace("Trace Message!");
             logger.debug("Debug Message!");
             logger.info("Info Message!");
@@ -27,11 +25,9 @@ public class MyCrawler {
     }
 
 //        mvn clean compile exec:java -Dexec.mainClass="com.yacht.MyCrawler" -Dorg.slf4j.simpleLogger.defaultLogLevel=INFO
-
-
     private static void basic() {
         List<? extends SiteCrawlerAction> siteCrawlerActions = new LinkedList<>();
-        SiteCrawler siteCrawler = new SiteCrawler("http://www.yacht.nl", "https://www.yacht.nl", siteCrawlerActions);
+        SiteCrawler siteCrawler = new SiteCrawler("www.ziggo.nl", "https://www.ziggo.nl", siteCrawlerActions);
 
         siteCrawler.getAllowedSuffixes().add("");
         siteCrawler.navigate();
--- src/main/java/com/yacht/TestIt.java
+++
@@ -1,4 +0,0 @@
-package com.yacht;
-
-public class TestIt {
-}
